abff2510ee9978197f575f509378964e82dd5e7b
kalpajp <kalpajyotinathprodhani@gmail.com>
2025-07-05 10:16:47 +0530

    portfoio done

---
+++ kalpa-s-Portfolio-main/src/constants.js
@@ -0,0 +1,261 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/eduAi.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/nits.webp';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/cotton.jpg';
+import skLogo from './assets/education_logo/skoool.jpg';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/tictactoe.png';
+import csprepLogo from './assets/work_logo/paisa.webp';
+import movierecLogo from './assets/work_logo/money.jpg';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/crypto.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+     // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+     // { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      //{ name: 'GSAP', logo: gsapLogo },
+      //{ name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      //{ name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      //{ name: 'Java', logo: javaLogo },
+      //{ name: 'Python', logo: pythonLogo },
+      //{ name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      //company: "Webverse Digital",
+      date: "june 2025 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        
+        " Next Js",
+      ],
+    },
+    
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "National Institute Of Technology Silchar",
+      date: "Sept 2022 - July 2026",
+      grade: "7.19 CGPA",
+      desc: "Hi, I'm a student from NIT Silchar, currently in the Electronics and Communication Engineering branch. While I've developed a strong base in electronics, my real interest lies in the tech and coding world. I enjoy writing code, exploring new technologies, and building software-based solutions—whether it’s for automating tasks, creating applications, or solving algorithmic challenges. I’m always eager to learn and work on projects that combine logic, creativity, and impact.",
+      degree: "Btech-Electronics and communication",
+    },
+    
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Cotton university, guwahati",
+      date: "Apr 2019 - March 2021",
+      grade: "95%",
+      desc: "I completed my class 12 education from cotton university ghy, under the AHSEC board, where I studied Physics, Chemistry, and Mathematics (PCM) .",
+      degree: "(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: skLogo,
+      school: "sankardev vidya niketan ,guwahati",
+      date: "Apr 2015 - March 2016",
+      grade: "90.83%",
+      desc: "I completed my class 10 education from sankardev vidya niketan ",
+      degree: "SEBA(X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "tic-tac-toe",
+      description:"Build a responsive 3 × 3 grid in HTML/CSS and wire up JavaScript click handlers to place “X”/“O”, check win‑conditions after each move, highlight the winning line, and offer a quick “Play again” reset button—yielding a fully playable Tic‑Tac‑Toe game entirely in the browser.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/kalpajp/tictactoe.git",
+      webapp: "",
+    },
+    {
+      id: 1,
+      title: "currency converter",
+      description:
+        " Create a clean input form with two currency dropdowns and amount fields, then use JavaScript to fetch real‑time exchange rates (e.g., via an open API), calculate the converted value on input change, display it instantly, and include basic CSS styling for a responsive, user‑friendly interface.",
+      image: csprepLogo,
+      tags: ["Api",  "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/kalpajp/currencyConverter.git",
+      webapp: "",
+    },
+    {
+      id: 2,
+      title: "currency converter using react",
+      description:
+        " Build a lightweight React component with two <select>s for currency codes and an <input> for amount, use useEffect + fetch (or Axios) to grab live rates from an open API when either currency/amount changes, compute & display the converted value instantly, and style the component responsively with simple CSS or a library like Tailwind.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/kalpajp/currency_converter2.git",
+      webapp: "https://currency-converter-snowy-pi.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "cryptoDashboard",
+      description:"\textbf{Rich finance UI:} responsive wallet overview showing total portfolio value in ₹, interactive price-chart tabs (1H/1D/1W/1M), recent transactions table with filters, and quick-action cards for deposits, withdrawals and contact. ",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "MongoDb", "Validation","Javascript","Tailwind"],
+      github: "https://github.com/kalpajp/-cryptoDashboard.git",
+      webapp: "",
+    },
+    /*{
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Coding Master",
+      description:
+        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "https://codingmasterweb.in/",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },*/
+  ];  
